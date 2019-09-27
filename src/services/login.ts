07e1731bb494a7b42f8bcdfeee8f4c43ba31be85
qinhan <qinhan@xindong.com>
2019-09-27 13:45:58 +0800

    feat(all): del usless file

--- src/services/login.ts
+++ src/services/login.ts
@@ -3,8 +3,6 @@ import request from '@/utils/request';
 export interface LoginParamsType {
   userName: string;
   password: string;
-  mobile: string;
-  captcha: string;
 }
 
 export async function fakeAccountLogin(params: LoginParamsType) {
